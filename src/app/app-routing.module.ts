4f8bc1437b9f9c7e682e40e4ed1ddba1666fed4f
greif.matthias <greif.matthias@outlook.com>
2018-11-19 13:29:38 +0100

    Added Rewards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminHomeComponent } from './admin/home/home.component';
+import { AdminRewardsComponent } from './admin/rewards/rewards.component';
 
 const routes: Routes = [
   {path: 'admin', component: AdminHomeComponent},
+  {path: 'admin/rewards/4', component: AdminRewardsComponent},
   {path: '', redirectTo: '', pathMatch: 'full'}, // Redirect default
 ];
 
