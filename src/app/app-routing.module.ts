1fdd10d1bec1b9a552955b00c5854567bc1f2239
greif.matthias <greif.matthias@outlook.com>
2018-11-19 09:15:18 +0100

    Added admin component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminHomeComponent } from './admin/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminHomeComponent},
+  {path: '', redirectTo: '', pathMatch: 'full'}, // Redirect default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './admin/home/home.component';
+import { AdminHomeComponent } from './admin/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
