8953b2be3159f1d8203f8fd13a5446dbf5c1896c
Brycevdb <vdb.bryce@gmail.com>
2018-11-20 09:41:09 +0100

    login page shows the usernames

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../services/user.service';
+import {User} from '../interfaces/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+  getUsers(): void {
+    this.userService.getUsers().subscribe(users => this.users = users);
   }
 
 }
