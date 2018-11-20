2c6bd33966be0b53a6d044f9c118651e05731733
Daan <daantjexx3@gmail.com>
2018-11-20 15:41:09 +0100

    testen user data

--- src/app/rewards/rewards.component.ts
+++ src/app/rewards/rewards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { RewardsService } from 'src/app/services/rewards.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-rewards',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rewards.component.scss']
 })
 export class RewardsComponent implements OnInit {
+  private user: User;
 
-  constructor() { }
+  constructor( public authService: AuthService, private rewardsservice: RewardsService) {
+    this.authService.userData$.subscribe(data => {
+      this.user = data;
+      console.log(data);
+    });
+  }
 
   ngOnInit() {
   }
